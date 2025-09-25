f1aa3a52ef53c3fd2e9c67931d6d147e2c05ece0
pat-nel87 <patnelson2022@gmail.com>
2025-09-25 19:57:31 -0400

    fix: remove duplicate output definitions from variables.tf
    Removes duplicate output blocks that were causing Terraform initialization errors.
    Outputs are now properly defined only in outputs.tf file.

--- modules/vnet/variables.tf
+++ modules/vnet/variables.tf
@@ -34,25 +34,3 @@ variable "tags" {
   type        = map(string)
   default     = {}
 }
-
-# modules/azure-simple-vnet/outputs.tf
-
-output "vnet_id" {
-  description = "ID of the virtual network"
-  value       = azurerm_virtual_network.this.id
-}
-
-output "vnet_name" {
-  description = "Name of the virtual network"
-  value       = azurerm_virtual_network.this.name
-}
-
-output "subnet_ids" {
-  description = "Map of subnet names to IDs"
-  value       = { for k, v in azurerm_subnet.subnets : k => v.id }
-}
-
-output "subnet_cidrs" {
-  description = "Map of subnet names to CIDR blocks"
-  value       = { for k, v in azurerm_subnet.subnets : k => v.address_prefixes[0] }
-}
